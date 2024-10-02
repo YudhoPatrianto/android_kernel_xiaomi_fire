a725696a7bb336e176927e3715f9ed2ea0defbcd
YudhoPatrianto <kydh01123@gmail.com>
2024-10-02 08:45:16 +0000

    aw87xxx_acf_bin.c: remove unused variable acf_dde
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- sound/soc/codecs/aw87xxx/aw87xxx_acf_bin.c
+++ sound/soc/codecs/aw87xxx/aw87xxx_acf_bin.c
@@ -280,7 +280,6 @@ static int aw_check_data_v_0_0_0_1(struct device *dev,
 static int aw_check_ddt_size_v_1_0_0_0(struct device *dev, char *fw_data)
 {
 	struct aw_acf_hdr *acf_hdr = (struct aw_acf_hdr *)fw_data;
-	struct aw_acf_dde_v_1_0_0_0 *acf_dde = NULL;
 
 	acf_dde = (struct aw_acf_dde_v_1_0_0_0 *)(fw_data + acf_hdr->ddt_offset);
 
